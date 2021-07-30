006742d7a6215499677a9b29e04237c3dc734fda
Matthew Macdonald-Wallace <matthew.macdonald-wallace@contino.io>
2021-07-30 15:30:15 +0100

    Update to XRay trace version

--- README.md
+++ README.md
@@ -12,6 +12,8 @@ Prometheus should scrape the `/metrics` endpoint of this application
 2. Activate the virtualenv
 3. Install the requirements: `pip install -r requirements.txt`
 4. Set the `OBSDEMO_OTLP_ENDPOINT` environment variable to point to your OTLP collector
-5. Run the app `python demo.py`
+5. Set the `OBSDEMO_APP_SECRET` environment variable to the value you want to use for your app secret key
+6. Assuming you're running in AWS, set `OTEL_PROPAGATORS=xray` to send traces to XRay
+7. Run the app `python demo.py`
 
 The app is now available at [http://localhost:5000/](http://localhost:5000/)
--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
-import os
 import boto3
 import random
-from flask import Flask, request, abort
+import time
+from flask import Flask, request, abort, session
 from prometheus_flask_exporter import PrometheusMetrics
 # Add imports for OTel components into the application
 from opentelemetry import trace
@@ -11,7 +11,8 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor
 # Import the AWS X-Ray for OTel Python IDs Generator into the application.
 from opentelemetry.sdk.extension.aws.trace import AwsXRayIdGenerator
 # Sends generated traces in the OTLP format to an ADOT Collector running on port 55678
-otlp_exporter = OTLPSpanExporter(endpoint=os.getenv("OBSDEMO_OTLP_ENDPOINT"), insecure=True)
+otlp_endpoint = os.getenv("OBSDEMO_OTLP_ENDPOINT") or "localhost:4317"
+otlp_exporter = OTLPSpanExporter(endpoint=otlp_endpoint, insecure=True)
 # Processes traces in batches as opposed to immediately one after the other
 span_processor = BatchSpanProcessor(otlp_exporter)
 # Configures the Global Tracer Provider
@@ -27,7 +28,58 @@ from opentelemetry.instrumentation.flask import FlaskInstrumentor
 BotocoreInstrumentor().instrument()
 
 
+##### SET XRAY FORMAT ####
+from opentelemetry.sdk.extension.aws.trace.propagation.aws_xray_format import (
+    TRACE_ID_DELIMITER,
+    TRACE_ID_FIRST_PART_LENGTH,
+    TRACE_ID_VERSION,
+)
+
+DIMENSION_API_NAME = "apiName"
+DIMENSION_STATUS_CODE = "statusCode"
+REQUEST_START_TIME = "requestStartTime"
+
+
+def convert_otel_trace_id_to_xray(otel_trace_id_decimal):
+    otel_trace_id_hex = "{:032x}".format(otel_trace_id_decimal)
+    x_ray_trace_id = TRACE_ID_DELIMITER.join(
+        [
+            TRACE_ID_VERSION,
+            otel_trace_id_hex[:TRACE_ID_FIRST_PART_LENGTH],
+            otel_trace_id_hex[TRACE_ID_FIRST_PART_LENGTH:],
+        ]
+    )
+    return '{{"traceId": "{}"}}'.format(x_ray_trace_id)
+
 app = Flask(__name__)
+app.secret_key=os.getenv("OBSDEMO_APP_SECRET")
+
+
+@app.before_request
+def before_request_func():
+    session[REQUEST_START_TIME] = int(time.time() * 1_000)
+
+@app.after_request
+def after_request_func(response):
+    # if request.path == "/outgoing-http-call":
+    #     apiBytesSentCounter.add(
+    #         response.calculate_content_length() + mimicPayloadSize(),
+    #         {
+    #             DIMENSION_API_NAME: request.path,
+    #             DIMENSION_STATUS_CODE: response.status_code,
+    #         },
+    #     )
+
+    #     apiLatencyRecorder.record(
+    #         int(time.time() * 1_000) - session[REQUEST_START_TIME],
+    #         {
+    #             DIMENSION_API_NAME: request.path,
+    #             DIMENSION_STATUS_CODE: response.status_code,
+    #         },
+    #     )
+
+    return response
+
 metrics = PrometheusMetrics(app)
 # Initialize instumentor for Flask web framework
 FlaskInstrumentor().instrument_app(app)
@@ -42,10 +94,14 @@ def main():
     with tracer.start_as_current_span("index_request"):
         rc = random.randrange(0,4)
         return_codes = [404, 401, 500, 502, 301]
-    if random.randrange(0, 100) > 85:
+    if random.randrange(0, 100) > 95:
         abort(return_codes[rc])
     else:
-        return "Working"
+        return app.make_response(
+                convert_otel_trace_id_to_xray(
+                    trace.get_current_span().get_span_context().trace_id
+                )
+            )
 
 if __name__ == "__main__":
     app.run()
